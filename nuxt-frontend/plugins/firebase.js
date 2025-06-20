0c848e8f806075db03c1e15c7e19d3a017cfa48a
yuriko <yuriko40223@gmail.com>
2025-06-20 18:41:35 +0900

    firebase関連

--- nuxt-frontend/plugins/firebase.js
+++ nuxt-frontend/plugins/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 
 export default (context, inject) => {
     const firebaseConfig = {
-        apiKey: "AIzaSyBAm0Ka32tC2LVgmE5kc9-fsrxtdIV94f8",
-        authDomain: "twitter-app-1b0d9.firebaseapp.com",
-        projectId: "twitter-app-1b0d9",
-        storageBucket: "twitter-app-1b0d9.firebasestorage.app",
-        messagingSenderId: "119235605003",
-        appId: "1:119235605003:web:5637e804f649489de00d0e"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
     };
     const app = initializeApp(firebaseConfig)
     const auth = getAuth(app)
